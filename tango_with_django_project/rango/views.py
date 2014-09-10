3144c3977eddd5d4265b585ebb51a6aea485611a
Abrahán Fernández Nieto <abrahanfer@gmail.com>
2014-09-10 19:44:05 +0200

    Adding exercises from chapter 3

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,9 @@
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Test with rango and gatitos :3")
+    return HttpResponse("Test with rango and gatitos :3 "
+    "<a href='/rango/about'>to about</a>")
+
+def about(request):
+    return HttpResponse("Rango Says: Here is the about page. "
+    "And <a href='/rango'>here</a> the index")
