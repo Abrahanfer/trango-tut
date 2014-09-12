f767cb0278eb02cd677f64a326a99029fbe0ff3d
Abrahán Fernández Nieto <abrahanfer@gmail.com>
2014-09-12 20:54:32 +0200

    Adding chapter 5 exercises

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+
+class PageAdmin(admin.ModelAdmin):
+    #fieldsets=[
+     #   ('Title', {'fields': ['title']}),
+      #  ('Category', {'fields': ['category']}),
+       # ('Url', {'fields': ['url']})
+    #]
+
+    list_display = ('title', 'category_name', 'url')
+
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     def _str_(self):
         return self.name
@@ -12,6 +14,10 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
+
+    def category_name(self):
+        return self.category.name
+    category_name.short_description = 'Category'
 
     def _str_(self):
         return self.title
